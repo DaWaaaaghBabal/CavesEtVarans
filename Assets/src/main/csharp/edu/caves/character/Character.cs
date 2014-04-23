addf139865456596195bfd407f3c4c31cbf3a223
J-S <dawaaaaghbabal@hotmail.fr>
2014-04-23 23:02:46 +0200

    Added a Gauge type for quality of life when manipulating AP, HP and other resources. Added statistics and an AP gauge to Character.

--- Assets/src/main/csharp/edu/caves/character/Character.cs
+++ Assets/src/main/csharp/edu/caves/character/Character.cs
@@ -4,15 +4,27 @@ namespace CavesEtVarans
 {
 	public class Character
 	{
-		public Character ()
-		{
-		
+        private Gauge AP;
+        private StatisticsManager statManager;
+
+		public Character () {
+            statManager = new StatisticsManager();
+            AP = new Gauge(0, statManager.getValue(Statistic.MAX_AP));
 		}
 		
 		public void Activate() {
 		
 		}
-		
+
+        public int GetAP() {
+            return AP.GetValue();
+        }
+        public void SetAP(int newValue) {
+            AP.SetValue(newValue);
+        }
+        public void IncrementAP(int newValue) {
+            AP.SetValue(AP.GetValue() + newValue);
+        }
 	}
 }
 
---
+++ Assets/src/main/csharp/edu/caves/utils/Gauge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavesEtVarans {
+    class Gauge {
+        private int min;
+        private int max;
+        private int value;
+
+        public Gauge(int newMin, int newMax) {
+            min = newMin;
+            max = newMax;
+            value = min;
+        }
+
+        public int GetValue() {
+            return value;
+        }
+
+        public void SetValue(int newValue) {
+            if (newValue < min) {
+                value = min;
+            } else if (newValue > max) {
+                value = max;
+            } else {
+                value = newValue;
+            }
+        }
+
+        public double getPercentage() {
+            return (value - min) / (max - min);
+        }
+    }
+}
