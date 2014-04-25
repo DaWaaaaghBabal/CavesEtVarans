d64ec537e922ea404dd1b4090859aa49b9830b81
Anastaszor <anastaszor@yahoo.fr>
2014-04-26 01:37:51 +0200

    Adding skillcost

---
+++ Assets/src/main/csharp/edu/caves/character/ressource/SkillCost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavesEtVarans
+{
+    class SkillCost
+    {
+        private Dictionary<string, int> elements;
+
+        protected Dictionary<string, int> getElements()
+        {
+            if (elements == null)
+            {
+                elements = new Dictionary<string, int>();
+            }
+            return elements;
+        }
+
+        public void Add(string key, int value) 
+        {
+            getElements().Add(key, value);
+        }
+
+        public int Get(string key)
+        {
+            if (getElements().ContainsKey(key))
+            {
+                return elements[key];
+            }
+            return 0;
+        }
+    }
+}
+
