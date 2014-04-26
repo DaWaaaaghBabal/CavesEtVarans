8df4e7d636d85853432a810208a5006d6b6d6687
J-S <dawaaaaghbabal@hotmail.fr>
2014-04-26 18:21:36 +0200

    Missed a bit ; now the abstract structure is in place.

--- Assets/src/main/csharp/edu/caves/skills/SkillCondition.cs
+++ Assets/src/main/csharp/edu/caves/skills/SkillCondition.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assets.src.main.csharp.edu.caves.skills {
+namespace CavesEtVarans {
     class SkillCondition {
-        public abstract void Test();
+
+        private string targetKey;
+        private string resultKey;
+        public void Test() {
+            HashSet<Character> targets = (HashSet<Character>)Context.Get(targetKey);
+            Dictionary<Character, ConditionResult> result = new Dictionary<Character, ConditionResult>();
+            foreach (Character target in targets) {
+                result.Add(target, TestTarget(target));
+            }
+            Context.Set(resultKey, result);
+        }
+
+        protected abstract ConditionResult TestTarget(Character target);
     }
 }
