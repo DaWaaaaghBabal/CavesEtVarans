c789781cac3c056b5adad3014126221039fa6e00
Anastaszor <anastaszor@yahoo.fr>
2014-04-26 21:18:15 +0200

    Adding observer pattern

---
+++ Assets/src/main/csharp/edu/caves/utils/IDisposable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavesEtVarans
+{
+    // Summary:
+    //     Defines a method to release allocated resources.
+    public interface IDisposable
+    {
+        // Summary:
+        //     Performs application-defined tasks associated with freeing, releasing, or
+        //     resetting unmanaged resources.
+        void Dispose();
+    }
+
+}
---
+++ Assets/src/main/csharp/edu/caves/utils/IObservable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CavesEtVarans
+{
+    // Summary:
+    //     Defines a provider for push-based notification.
+    //
+    // Type parameters:
+    //   T:
+    //     The object that provides notification information.This type parameter is
+    //     covariant. That is, you can use either the type you specified or any type
+    //     that is more derived. For more information about covariance and contravariance,
+    //     see Covariance and Contravariance in Generics.
+    public interface IObservable<out T>
+    {
+        // Summary:
+        //     Notifies the provider that an observer is to receive notifications.
+        //
+        // Parameters:
+        //   observer:
+        //     The object that is to receive notifications.
+        //
+        // Returns:
+        //     A reference to an interface that allows observers to stop receiving notifications
+        //     before the provider has finished sending them.
+        IDisposable Subscribe(IObserver<T> observer);
+    }
+
+}
