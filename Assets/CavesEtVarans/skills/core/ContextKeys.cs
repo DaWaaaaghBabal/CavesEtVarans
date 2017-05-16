9beb7e9b614cccb898efae99398b2e1ea00a4fde
DaWaaaaghBabal <dawaaaaghbabal@hotmail.fr>
2017-05-16 18:47:26 +0200

    (See previous commit, added files were unchecked)

---
+++ Assets/CavesEtVarans/skills/core/ContextKeys.cs
@@ -0,0 +1,22 @@
+﻿namespace CavesEtVarans.skills.core
+{
+	
+	public static class ContextKeys
+    {
+		public static readonly string SOURCE = "source";
+        public static readonly string TARGETS = "targets";
+        public static readonly string CURRENT_TARGET = "currentTarget";
+        public static readonly string MOVEMENT_TARGET = "mvtTarget";
+		public static readonly string BUFF_SOURCE = "buffSource";
+		public static readonly string BUFF_TARGET = "buffTarget";
+		public static readonly string SKILL = "skill";
+		public static readonly string TILE = "tile";
+        public static readonly string DAMAGE_TYPE = "damageType";
+		public static readonly string TRIGGERING_SKILL = "triggeringSkill";
+		public static readonly string TRIGGERING_TARGETS = "triggeringTarget";
+		public static readonly string TRIGGERING_CHARACTER = "triggeringCharacter";
+        public static readonly string FILTER_TARGET = "filterTarget";
+		public static readonly string START_TILE = "startTile";
+		public static readonly string END_TILE = "endTile";
+    }
+}
---
+++ Assets/CavesEtVarans/skills/core/IContextDependent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CavesEtVarans.skills.core {
+    public interface IContextDependent {
+        ContextDependent Parent { get; set; }
+        ContextDependent Clone();
+        void SetLocalContext(Dictionary<string, object> contextData);
+        void SetLocalContext(string key, object value);
+    }
+}
